cff68a2fcad286a2302faf8021cf119264f2613b
alexykot <alexykot@gmail.com>
2016-04-28 23:54:15 +0100

    - completely purged spkw/dist/ - build tasks added - switched from global to local builder installations - builders done

--- spkw/builder/init.sh
+++ spkw/builder/init.sh
@@ -3,7 +3,7 @@
 OWN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker stop speckle-viewer-dev
-#docker rm -v speckle-viewer-dev
+docker rm -v speckle-viewer-dev
 docker build --rm=true -t "local:nodebase" ${OWN_DIR}/nodebase
 docker build --rm=true -t "local:speckle-viewer-dev" ${OWN_DIR}/viewer-dev
 docker create --name speckle-viewer-dev -p 8080:8080 \
